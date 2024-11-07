e543243c0dcaa04605a1086d2748a5a173da2bfe
Lukas Aebi <lukas.aebi@gmx.net>
2024-11-07 11:07:06 +0100

    created scripts to create and delete s3 buckets

---
+++ s3_buckets/create_bucket.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Catch case where user not providing bucket name
+if [ -z "$1" ]; then
+    echo "Please provide a name for the S3 bucket"
+    exit 1
+# Catch underscores in bucket name
+elif [[ $1 == *"_"* ]]; then
+    echo "Bucket names must not contain underscores!"
+    exit 1
+fi
+
+echo "Creating a new S3 bucket with name: $1"
+
+aws s3api create-bucket \
+    --bucket "$1" \
+    --region us-east-1
+
+# Run with: bash path/to/create_bucket.sh <bucket-name>
