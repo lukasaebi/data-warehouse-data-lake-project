0474d331ec2a2ca5319b56136206cdd6624c6f42
Lukas Aebi <lukas.aebi@gmx.net>
2024-11-08 14:21:04 +0100

    changed $1 to explicit variable name BUCKET_NAME in create_bucket.sh

--- s3_buckets/create_bucket.sh
+++ s3_buckets/create_bucket.sh
@@ -2,20 +2,22 @@
 
 set -e
 
+BUCKET_NAME=$1
+
 # Catch case where user not providing bucket name
-if [ -z "$1" ]; then
+if [ -z "$BUCKET_NAME" ]; then
     echo "Please provide a name for the S3 bucket"
     exit 1
 # Catch underscores in bucket name
-elif [[ $1 == *"_"* ]]; then
+elif [[ $BUCKET_NAME == *"_"* ]]; then
     echo "Bucket names must not contain underscores!"
     exit 1
 fi
 
-echo "Creating a new S3 bucket with name: $1"
+echo "Creating a new S3 bucket with name: $BUCKET_NAME"
 
 aws s3api create-bucket \
-    --bucket "$1" \
+    --bucket "$BUCKET_NAME" \
     --region us-east-1
 
 # Run with: bash path/to/create_bucket.sh <bucket-name>
